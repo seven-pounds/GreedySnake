1c05bfb83636c2597a6940b8f6ba51d8226b7555
jzfan <jzfan@trip.com>
2023-06-14 10:35:41 +0800

    create repository

---
+++ src/test/java/com/example/greedysnake/GreedySnakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greedysnake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreedySnakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
